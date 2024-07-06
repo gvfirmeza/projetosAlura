3bc8828792d3d873d5aa73eb509f19bba18aeb12
gvfirmeza <gvfirmeza@gmail.com>
2024-07-06 16:42:50 -0300

    estudando .NET

---
+++ dotNET/FilmesAPI/FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,22 @@
+using FilmesApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+
+public class FilmeController : ControllerBase
+{
+
+    private List<Filme> filmes = new List<Filme>();
+
+    [HttpPost]
+    public void AdicionaFilme([FromBody] Filme filme)
+    {
+        filmes.Add(filme);
+        Console.WriteLine(filme.Titulo);
+        Console.WriteLine(filme.Genero);
+        Console.WriteLine(filme.Duracao);
+    }
+}
